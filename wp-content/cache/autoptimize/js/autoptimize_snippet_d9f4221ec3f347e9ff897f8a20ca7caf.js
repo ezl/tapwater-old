053d2fad6a3c0cf82013e583e94266c34802c63c
Filip <filipmarkovic@gmail.com>
2020-04-16 15:13:53 -0700

    a

--- wp-content/cache/autoptimize/js/autoptimize_snippet_d9f4221ec3f347e9ff897f8a20ca7caf.js
+++
@@ -1,3 +0,0 @@
-(function($){$(document).ready(function(){$(window).on('load resize',function(){var sidebarH=$('#sidebar').height();$('.mobile-search').css('top',sidebarH);}).resize();$('.dropdown-toggle').on('click',function(){var sidebarH=$('#sidebar').height();$('.mobile-search').css('top',sidebarH);});});if($('.table-bool').html()=='yes'){$('.table-bool').css('color','green');}
-if($('.table-bool').html()=='no'){$('.table-bool').css('color','red');}
-$('.subcategory-nav-item').on('click',function(){if($(this).hasClass('active')){}else{$('.subcategory-nav-item').removeClass('active');$(this).addClass('active');var region=$(this).attr("data-region");$(".region").removeClass('active');$('.region[data-region="'+region+'"]').addClass('active');}});})(jQuery);
